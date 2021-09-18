c05945b219fcad7245b92593072569235b71a978
Stefan O'Neil <oneil.stefan@gmail.com>
2021-09-18 16:16:21 +0900

    Worked on examples.

---
+++ JavaScript/Output/myScript.js
@@ -0,0 +1,7 @@
+function innerHTMLFunction() {
+  document.getElementById("innerHTMLDemo").innerHTML =
+    "This was shown using innerHTML";
+}
+function documentwrite() {
+  document.write("Congrats, you destroyed everything");
+}
